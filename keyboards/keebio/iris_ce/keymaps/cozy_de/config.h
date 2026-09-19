07b510eb13e7e71327dcfa40a4f55707dd85f102
Robert Will <matey.jack@gmail.com>
2026-09-19 15:22:41 +0200

    add comment

--- keyboards/keebio/iris_ce/keymaps/cozy_de/config.h
+++ keyboards/keebio/iris_ce/keymaps/cozy_de/config.h
@@ -24,6 +24,8 @@
 #define RGB_MATRIX_SLEEP               // turn off effects when suspended
 
 // RGB_MATRIX_SOLID_COLOR ==> is always on (has no ENABLE_ flag)
+
+// This flag only works when compiling the keymap against my own fork of QMK which has this new color mode.
 #define ENABLE_RGB_MATRIX_KEY_GROUPS
 
 // Disable all other animations so that we can simply toggle between the solid color and the mods&toggles.
